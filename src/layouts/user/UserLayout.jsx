4cd41ea4a3a21a9587c2946602372ea23c6e9a13
minh hieu <minhhieudev@gmail.com>
2025-03-13 09:59:48 +0700

    fix

--- src/layouts/user/UserLayout.jsx
+++ src/layouts/user/UserLayout.jsx
@@ -4,12 +4,16 @@ import Header from "../../components/user/header/Header";
 
 const UserLayout = () => {
     return (
-        <div className="user-layout">
-            <div className="fixed top-0 left-0 right-0 z-50">
-                <Header />
+        <div className="user-layout min-h-screen bg-gradient-to-r from-[#FFFAFA] to-[#f3faf3]">
+            <div className="fixed top-0 left-0 right-0 z-50 bg-white">
+                <div className="max-w-[1440px] mx-auto">
+                    <Header />
+                </div>
             </div>
-            <div className="user-content pt-12">
-                <Outlet />
+            <div className="user-content pt-[60px]">
+                <div className="max-w-[1440px] mx-auto">
+                    <Outlet />
+                </div>
             </div>
         </div>
     );

411425da480b13fc97df0e264d2a7d5ea464e779
minh hieu <minhhieudev@gmail.com>
2025-03-17 20:21:23 +0700

    up

--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -6,7 +6,7 @@ const ProtectedRoute = ({ children }) => {
     const userInfo = JSON.parse(localStorage.getItem(CONST.STORAGE.USER_INFO));
 
     const isAdmin = true;
-    // const isAdmin = userInfo && userInfo.role === true;
+    // const isAdmin = userInfo && userInfo.role === true;lllllllll
 
     return isAdmin ? children : <Navigate to="/" />;
 };
